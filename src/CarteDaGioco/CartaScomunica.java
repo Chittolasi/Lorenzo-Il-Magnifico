493f194b3b727505ebce90f1bf9877af3c2d26fe
PietroChitto <pietrochitto@hotmail.it>
2017-05-11 18:08:13 +0200

    Update CartaScomunica.java

--- src/CarteDaGioco/CartaScomunica.java
+++ src/CarteDaGioco/CartaScomunica.java
@@ -1,11 +1,33 @@
-package CarteDaGioco;
+package carteDaGioco;
+
+import componentiDelTabellone.Giocatore;
 
 /**
- * Created by william on 10/05/2017.
+ * Created by Pietro on 11/05/2017.
  */
 public class CartaScomunica {
 
+    private int id;
+    private String effetto;
+    private int periodo;
 
 
+    public CartaScomunica (int id, String effetto, int periodo){
+
+        this.periodo=periodo;
+        this.id=id;
+        this.effetto=effetto;
+
+    }
+
+    public int getPeriodo(){
+        return periodo;
+    }
+
+    public void attivaEffettoScomunica(Giocatore giocatore){
+
+
+
+    }
 
 }
