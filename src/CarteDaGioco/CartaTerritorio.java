5c2da2e615f7a260b665932e8006d2bd20748442
PietroChitto <pietrochitto@hotmail.it>
2017-05-11 18:10:30 +0200

    Update CartaTerritorio.java

--- src/CarteDaGioco/CartaTerritorio.java
+++ src/CarteDaGioco/CartaTerritorio.java
@@ -1,21 +1,50 @@
-package CarteDaGioco;
-
-import CarteDaGioco.CartaSviluppo;
+import componentiDelTabellone.Giocatore;
 
 /**
  * Created by william on 10/05/2017.
  */
-public class CartaTerritorio extends CartaSviluppo {
+public class CartaTerritorio extends CartaSviluppo{
 
     private String nomeEffettoPermanente;
     private String nomeEffettoRapido;
+    private int costoAttivazioneEffettoPermanente;
 
-    public CartaTerritorio(int periodo, String nome, boolean effettoRapido, boolean effettoPermanente, String nomeEffettoRapido, String nomeEffettoPermanente){
+    public CartaTerritorio(int periodo, String nome, boolean effettoRapido, boolean effettoPermanente, String nomeEffettoRapido, String nomeEffettoPermanente, int costoAttivazioneEffettoPermanente){
 
-        super(periodo, nome, effettoRapido, effettoPermanente);
+        super(periodo, nome, effettoRapido, effettoPermanente, "territorio");
         this.nomeEffettoPermanente=nomeEffettoPermanente;
         this.nomeEffettoRapido=nomeEffettoRapido;
+        this.costoAttivazioneEffettoPermanente=costoAttivazioneEffettoPermanente;
 
+    }
+
+    /**
+     * controlla se la carta ha un effetto rapido e si occupa di chiamare il metodo che implementa l'effetto
+     * @param g: giocatore possessore della carta
+     */
+    public void attivaEffettoRapido(Giocatore g){
+        if(super.getEffettoRapido()){
+            //cerca meteodo rapido
+        }
+    }
+
+    /**
+     * controlla se la carta ha un effetto permanente e si occupa dell'attivazione del metodo che implementa l'effetto
+     * @param f: familiare del giocatore possessore della carta
+     */
+    public void attivaEffettoPermanente(Familiare f){
+        if(super.getEffettoPermanente() && verificaDisponibilitàForza(f.getForza())){
+            //cerca metodo permanente
+        }
+    }
+
+    /**
+     * verifica se il familiare che attiva le carte edificio ha la forza necessaria per attivare l'effetto
+     * @param forza: forza del familiare che vuole attivare l'effetto
+     * @return true se la forza è maggiore o uguale; false altrimenti
+     */
+    public boolean verificaDisponibilitàForza(int forza){
+        if(forza>=costoAttivazioneEffettoPermanente) return true; return false;
     }
 
 }
