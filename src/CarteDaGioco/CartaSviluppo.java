f4c1746ac62f0cf907c74fece3a82a65c88fcda6
PietroChitto <pietrochitto@hotmail.it>
2017-05-11 18:09:32 +0200

    Update CartaSviluppo.java

--- src/CarteDaGioco/CartaSviluppo.java
+++ src/CarteDaGioco/CartaSviluppo.java
@@ -1,4 +1,4 @@
-package CarteDaGioco;
+package carteDaGioco;
 
 /**
  * Created by william on 10/05/2017.
@@ -9,14 +9,36 @@ public abstract class CartaSviluppo {
     private String nome;
     private boolean effettoRapido;
     private boolean effettoPermanente;
+    private String tipo;
 
-    public CartaSviluppo(int periodo, String nome, boolean effettoRapido, boolean effettoPermanente){
+    public CartaSviluppo(int periodo, String nome, boolean effettoRapido, boolean effettoPermanente, String tipo){
 
         this.effettoPermanente=effettoPermanente;
         this.effettoRapido=effettoRapido;
         this.nome=nome;
         this.periodo=periodo;
+        this.tipo=tipo;
 
+    }
+
+    public int getPeriodo(){
+        return periodo;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public boolean getEffettoRapido(){
+        return effettoRapido;
+    }
+
+    public boolean getEffettoPermanente(){
+        return effettoPermanente;
+    }
+
+    public String getTipo(){
+        return tipo;
     }
 
 }
