1c69fc5c61dd8feb51fe266c691f1389c4d6cf25
PietroChitto <pietrochitto@hotmail.it>
2017-05-11 18:06:26 +0200

    Update CartaImpresa.java

--- src/CarteDaGioco/CartaImpresa.java
+++ src/CarteDaGioco/CartaImpresa.java
@@ -1,25 +1,48 @@
-package CarteDaGioco;
+package carteDaGioco;
+
+import componentiDelTabellone.Giocatore;
 
 /**
  * Created by william on 10/05/2017.
  */
 public class CartaImpresa extends CartaSviluppo {
 
-    private String nomeEffettoPermanente;
     private String nomeEffettoRapido;
-    private CostoCarta costoCarta1;
+    private CostoCarta costoCarta1;//le carte impresa possono avere due costi tra cui scegliere
     private CostoCarta costoCarta2;
     private boolean doppioCosto;
+    private int puntiVittoria;//punti vottoria che la carta darà a fine partita al giocatore
 
-   public CartaImpresa(CostoCarta costo1, CostoCarta costo2, boolean doppioCosto, int periodo, String nome, boolean effettoRapido, boolean effettoPermanente, String nomeEffettoRapido, String nomeEffettoPermanente){
+    public CartaImpresa(CostoCarta costo1, CostoCarta costo2, boolean doppioCosto, int periodo, String nome, boolean effettoRapido, String nomeEffettoRapido){
 
-        super(periodo, nome, effettoRapido, effettoPermanente);
-        this.nomeEffettoPermanente=nomeEffettoPermanente;
+        super(periodo, nome, effettoRapido, false, "Impresa");
         this.nomeEffettoRapido=nomeEffettoRapido;
-        this.costoCarta1=costoCarta1;
-        this.costoCarta2=costoCarta2;
+        this.costoCarta1=costo1;
+        this.costoCarta2=costo2;
         this.doppioCosto=doppioCosto;
 
-   }
+    }
+
+    public CostoCarta getCosto1(){
+        return costoCarta1;
+    }
+
+    public CostoCarta getCosto2(){
+        return costoCarta2;
+    }
+
+    public int getPuntiVittoria(){
+        return puntiVittoria;
+    }
+
+    /**
+     * controlla se la carta ha un effetto rapido e si occupa di chiamare il metodo che implementa l'effetto
+     * @param g: giocatore possessore della carta
+     */
+    public void attivaEffettoRapido(Giocatore g){
+        if(super.getEffettoRapido()){
+            //cerca meteodo rapido
+        }
+    }
 
 }
