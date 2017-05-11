f9cc91ce693ed2464a9860c3d58a10a69bf8332b
PietroChitto <pietrochitto@hotmail.it>
2017-05-11 14:57:21 +0200

    Update Vaticano.java

--- src/ComponentiDelTabellone/Vaticano.java
+++ src/ComponentiDelTabellone/Vaticano.java
@@ -1,6 +1,6 @@
-package ComponentiDelTabellone;
+package componentiDelTabellone;
 
-import CarteDaGioco.CartaScomunica;
+import carteDaGioco.CartaScomunica;
 
 /**
  * Created by Pietro on 10/05/2017.
@@ -8,11 +8,11 @@ import CarteDaGioco.CartaScomunica;
 public class Vaticano {
     private CartaScomunica[] carteScomunica;
 
-    /*
-    Il costruttore prende in ingresso le 3 cartye scomunica e le salva nell'array;
-     */
-    public Vaticano(CartaScomunica periodo1, CartaScomunica periodo2, CartaScomunica periodo3){
+    public Vaticano(){
         carteScomunica=new CartaScomunica[3];
+    }
+
+    public void setCarteScomunica(CartaScomunica periodo1, CartaScomunica periodo2, CartaScomunica periodo3){
         carteScomunica[0]=periodo1;
         carteScomunica[1]=periodo2;
         carteScomunica[2]=periodo3;
